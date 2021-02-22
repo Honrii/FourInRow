8d0d58906e756acdb35cede2a832a2ddc2a090c7
HONRADO\usuario <dhonradoma@gmail.com>
2021-02-22 15:10:19 +0100

    2nd commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/app/view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("4inRow");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
--- src/app/controller/Controller.java
+++ src/app/controller/Controller.java
@@ -2,6 +2,7 @@ package app.controller;
 
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 
 import javafx.scene.Scene;
@@ -10,6 +11,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+
+import java.lang.reflect.Array;
 
 import static javafx.application.Application.launch;
 
@@ -22,27 +25,80 @@ public class Controller {
 
     GridPane tablero = new GridPane();
     Scene tableroScene;
+    int columns = 6;
+    int rows = 7;
+    int [][] matriz = new int[7][6];
+    int[][] tableroJuego = new int[rows][columns];
+
 
 
     @FXML
     void generarTablero(ActionEvent event) {
-        int columns = 6;
-        int rows = 7;
-        for (int y = 0; y < columns; y++) {
-            for (int x = 0; x < rows; x++) {
+        llenarTablero();
+        int buttonSize = 70;
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < columns; x++) {
 
-                TextField tf = new TextField();
-                tf.setPrefHeight(50);
-                tf.setPrefWidth(50);
-                tablero.setRowIndex(tf, y);
-                tablero.setColumnIndex(tf, x);
-                tablero.getChildren().add(tf);
+                Button button = new Button();
+                button.setMinSize(buttonSize, buttonSize);
+                button.setPrefSize(buttonSize, buttonSize);
+                button.setMaxSize(buttonSize, buttonSize);
+                button.setId(y+","+x);
+
+                if(y==0) {
+                    button.setOnAction(
+                            new EventHandler<ActionEvent>() {
+                                @Override
+                                public void handle(ActionEvent actionEvent) {
+                                    System.out.println(((Button) actionEvent.getSource()).getId());
+
+
+                                    Button buttontoChange = (Button) tableroScene.lookup("#"+espacioLibre(((Button) actionEvent.getSource()).getId()));
+                                    buttontoChange.setStyle("-fx-background-color: #008000;");
+
+
+
+                                }
+                            });
+                }
+                tablero.setRowIndex(button, y);
+                tablero.setColumnIndex(button, x);
+                tablero.getChildren().add(button);
+
             }
+
+
         }
+
         tableroScene = new Scene(tablero, 500, 500);
         Stage stage = ((Stage)((Button)(event.getSource())).getScene().getWindow());
         stage.setScene(tableroScene);
     }
+
+    private void llenarTablero(){
+        for (int i = 0; i <tableroJuego.length ; i++) {
+            for (int j = 0; j <tableroJuego[i].length ; j++) {
+              tableroJuego[i][j]=0;
+            }
+
+        }
+    }
+    private String espacioLibre(String idButn){
+        int colum = Integer.parseInt( idButn.split(",")[1]);
+        for (int i = 0; i < tableroJuego.length; i++) {
+            if (tableroJuego[i][colum]==1){
+                tableroJuego[i-1][colum]=1;
+                return (i-1)+","+colum;
+            }
+        }
+        tableroJuego[rows-1][colum]=1;
+        return (rows-1)+","+colum;
+
+    }
+
+
+
+
 
     public static void main(String[] args) {
         launch(args);
